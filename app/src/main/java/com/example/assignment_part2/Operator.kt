ad134a83551f7b2f8c01e8a4e66ad6a8c295394d
Kyaw Thu <evanch98@gmail.com>
2023-07-04 00:32:21 +0800

    add a new interface Operator

--- app/src/main/java/com/example/assignment_part2/Operator.kt
+++ app/src/main/java/com/example/assignment_part2/Operator.kt
@@ -1,4 +1,8 @@
 package com.example.assignment_part2
 
 interface Operator {
+  fun add(num1: Double, num2: Double): Double
+  fun subtract(num1: Double, num2: Double): Double
+  fun multiply(num1: Double, num2: Double): Double
+  fun divide(num1: Double, num2: Double): Double
 }
