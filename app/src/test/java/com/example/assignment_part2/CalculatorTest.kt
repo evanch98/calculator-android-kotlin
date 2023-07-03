d5e43c30a6d909ef1bd52e013949a7884d1b0d99
Kyaw Thu <evanch98@gmail.com>
2023-07-04 00:44:26 +0800

    add a new test to test the initial state

--- app/src/test/java/com/example/assignment_part2/CalculatorTest.kt
+++ app/src/test/java/com/example/assignment_part2/CalculatorTest.kt
@@ -1,3 +1,23 @@
 package com.example.assignment_part2
 
-internal class CalculatorTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+private const val DELTA = 1e-15
+
+class CalculatorTest {
+
+  private lateinit var calculator: Calculator
+
+  @Before
+  fun initialize() {
+    calculator = Calculator();
+  }
+
+  @Test
+  fun testInitialCondition() {
+    val result = calculator.getValue()
+    assertEquals(0.0, result, DELTA)
+  }
+}
