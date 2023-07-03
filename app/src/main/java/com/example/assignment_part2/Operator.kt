3fa6faaa5883a5edb6a97775d01746b526d2ade3
Kyaw Thu <evanch98@gmail.com>
2023-07-04 00:47:05 +0800

    update the Operator interface

--- app/src/main/java/com/example/assignment_part2/Operator.kt
+++ app/src/main/java/com/example/assignment_part2/Operator.kt
@@ -1,8 +1,8 @@
 package com.example.assignment_part2
 
 interface Operator {
-  fun add(num1: Double, num2: Double): Double
-  fun subtract(num1: Double, num2: Double): Double
-  fun multiply(num1: Double, num2: Double): Double
-  fun divide(num1: Double, num2: Double): Double
+  fun add(num1: Double, num2: Double)
+  fun subtract(num1: Double, num2: Double)
+  fun multiply(num1: Double, num2: Double)
+  fun divide(num1: Double, num2: Double)
 }
