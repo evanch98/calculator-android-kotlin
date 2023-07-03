987a188f8b7d4882077faf8356b21f8dc8ca2791
Kyaw Thu <evanch98@gmail.com>
2023-07-04 00:35:29 +0800

    add a new class Calculator

--- app/src/main/java/com/example/assignment_part2/Calculator.kt
+++ app/src/main/java/com/example/assignment_part2/Calculator.kt
@@ -1,4 +1,29 @@
 package com.example.assignment_part2
 
-class Calculator {
+class Calculator: Operator {
+  private var value: Double = 0.0
+
+  private fun setValue(value: Double) {
+    this.value = value
+  }
+
+  fun getValue(): Double {
+    return this.value
+  }
+
+  override fun add(num1: Double, num2: Double): Double {
+    TODO("Not yet implemented")
+  }
+
+  override fun subtract(num1: Double, num2: Double): Double {
+    TODO("Not yet implemented")
+  }
+
+  override fun multiply(num1: Double, num2: Double): Double {
+    TODO("Not yet implemented")
+  }
+
+  override fun divide(num1: Double, num2: Double): Double {
+    TODO("Not yet implemented")
+  }
 }
